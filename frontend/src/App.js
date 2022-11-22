3a70623eab0cb4d6dd8bea46ba70c05789b94c51
Rishabh Thakur <74073486+rishabhthakur11@users.noreply.github.com>
2022-11-22 14:22:24 +0530

    frontend created

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Form } from "./components/Form";
+import { UserList } from "./components/UserList";
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
